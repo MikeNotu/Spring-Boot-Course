884ad48c27beae1eb2058d42ac721ff7eb514731
MikeNotu <michael.bateman@globant.com>
2022-10-18 15:49:12 -0500

    Student demo

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -14,8 +19,16 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public String hello() {
-		return "Hello World";
+	public List<Student> hello() {
+		return List.of(
+				new Student(
+						1L,
+						"Mike",
+						"michael@gmail.com",
+						LocalDate.of(1998, Month.MAY,5),
+						24
+				)
+		);
 	}
 
 }
