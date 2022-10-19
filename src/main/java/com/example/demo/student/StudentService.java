74db9d9f963a11db0a704b9e3593342c97b1b0ce
MikeNotu <michael.bateman@globant.com>
2022-10-19 16:36:58 -0500

    min 32

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,4 +1,25 @@
 package com.example.demo.student;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
 public class StudentService {
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Mike",
+                        "michael@gmail.com",
+                        LocalDate.of(1998, Month.MAY, 5),
+                        24
+                )
+        );
+
+
+    }
 }
