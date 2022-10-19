b883d980bbe342c5b8f0969ea211ce56c828015a
MikeNotu <michael.bateman@globant.com>
2022-10-19 15:38:36 -0500

    Student Controller Finished

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,4 +1,28 @@
 package com.example.demo.student;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
 public class StudentController {
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Mike",
+                        "michael@gmail.com",
+                        LocalDate.of(1998, Month.MAY,5),
+                        24
+                )
+        );
+    }
+
 }
